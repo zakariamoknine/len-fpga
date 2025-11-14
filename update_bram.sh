f8d280105076abea89aab82d4d7cab736378e60c
zakariamoknine <zakariamoknine3@gmail.com>
2025-11-14 10:39:12 +0100

    Initial commit

--- update_bram.sh
+++ update_bram.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-updatemem -meminfo blk_mem_gen_0.mmi -data ../len-firmware/out/firmware.mem -bit len-fpga.runs/impl_1/topbd_wrapper.bit -proc dummy -out len-fpga.runs/impl_1/topbd_wrapper_with_data.bit -force
+updatemem -meminfo blk_mem_gen_0.mmi \
+	  -data ../len-firmware/out/firmware.mem \
+	  -bit len-fpga.runs/impl_1/topbd_wrapper.bit \
+	  -proc dummy \
+	  -out len-fpga.runs/impl_1/topbd_wrapper_with_data.bit \
+	  -force
