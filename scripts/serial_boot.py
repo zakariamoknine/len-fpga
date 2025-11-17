91934a1743298dfc5c94a3e12d38dab7049ddf7f
zakariamoknine <zakariamoknine3@gmail.com>
2025-11-17 20:00:35 +0100

    adapt to new file names

--- scripts/serial_boot.py
+++ scripts/serial_boot.py
@@ -1,24 +1,66 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import serial
+import struct
+import time
+import sys
 
 SERIAL_PORT = "/dev/ttyUSB1"
 BAUDRATE = 115200
-FIRMWARE = "build/opensbi.bin"
+PAYLOAD_FILE = "build/opensbi/platform/lenfpga/firmware/fw_payload.bin"
 
-print(f"Opening {SERIAL_PORT}...")
-ser = serial.Serial(SERIAL_PORT, BAUDRATE, timeout=1)
+SERIAL_MAGIC = 0x0dca18fb
+LOAD_ADDR = 0x80000000
+ENTRY_ADDR = 0x80000000
 
-print(f"Loading '{FIRMWARE}'...")
-with open(FIRMWARE, "rb") as f:
-    data = f.read()
+def main():
+    with open(PAYLOAD_FILE, "rb") as f:
+        payload = f.read()
 
-print(f"Sending {len(data)} bytes...")
-ser.write(data)
-ser.flush()
+    payload_size = len(payload)
+    print(f"Payload size: {payload_size} bytes")
 
-print("Done!")
-while True:
-    line = ser.readline()
-    if line:
-        print(line.decode(errors="ignore"), end="")
+    #
+    # firmware/serial.h:
+    #
+    # struct serial_header {
+    #     uint32_t magic;
+    #     uint32_t size;
+    #     uint32_t load_addr;
+    #     uint32_t entry_addr;
+    # }
+    #
+    header = struct.pack("<IIII", SERIAL_MAGIC, payload_size, LOAD_ADDR, ENTRY_ADDR)
+
+    with serial.Serial(SERIAL_PORT, BAUDRATE, timeout=1) as ser:
+        print(f"Sending header...")
+        ser.write(header)
+        ser.flush()
+
+        time.sleep(1);
+
+        print("Sending payload...")
+        chunk_size = 1024
+        total_sent = 0
+
+        for i in range(0, payload_size, chunk_size):
+            chunk = payload[i:i+chunk_size]
+            ser.write(chunk)
+            ser.flush()
+            total_sent += len(chunk)
+
+            percent = (total_sent * 100) // payload_size
+            print(f"\rProgress: {percent}%", end="", flush=True)
+
+            time.sleep(0.001)
+
+        print("\nDone sending payload!")
+
+        while True:
+            data = ser.read(256)
+            if data:
+                sys.stdout.write(data.decode("latin1", errors="replace"))
+                sys.stdout.flush()
+
+if __name__ == "__main__":
+    main()
--- scripts/update_bram.sh
+++ scripts/update_bram.sh
@@ -4,5 +4,5 @@ updatemem -meminfo blk_rom.mmi \
 	  -data $1 \
 	  -bit len-fpga.runs/impl_1/topbd_wrapper.bit \
 	  -proc dummy \
-	  -out len-fpga.runs/impl_1/topbd_wrapper_with_data.bit \
+	  -out len-fpga.runs/impl_1/topbd_wrapper_with_init_rom.bit \
 	  -force
