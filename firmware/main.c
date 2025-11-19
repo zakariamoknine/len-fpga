9bf30e281fdd25ec78fc210635d1b8145051f52e
zakariamoknine <zakariamoknine3@gmail.com>
2025-11-19 22:58:13 +0100

    update firmware

--- firmware/main.c
+++ firmware/main.c
@@ -1,11 +1,10 @@
 #include "print.h"
 #include "serial.h"
 
-void start(void)
+void start()
 {
 	uart_init(115200, 100000000);
-
-	print("Hello from FPGA!\n\n");
+	print("len-fpga booting...");
 
 	serial_load(true);
 
